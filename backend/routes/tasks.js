66987878bb85c47a6c5ec7284cf2ccc8a26ebd54
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-09-15 20:59:48 +0200

    Removed the tasks

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,27 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import { 
-    getTaskById,
-    getTasks,
-    createTask,
-    updateTask,
-    deleteTask
-} from '../controllers/tasksController.js';
 
 
-// Getting tasks and with their limits
-router.get('/', getTasks);
 
-// Getting tasks by their id
-router.get('/:id', getTaskById);
 
-// Creating task
-router.post('/', createTask);
-
-// Updating tasks
-router.put('/:id', updateTask);
-
-// Deleting user
-router.delete('/:id', deleteTask);
 
 export default router;
