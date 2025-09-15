d624146cae83372fc94f040835d4ba1bb3655adc
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-09-15 21:01:21 +0200

    Added the task controllers

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -9,6 +9,19 @@ import {
 } from '../controllers/tasksController.js';
 
 
+// Getting tasks and with their limits
+router.get('/', getTasks);
 
+// Getting tasks by their id
+router.get('/:id', getTaskById);
+
+// Creating task
+router.post('/', createTask);
+
+// Updating tasks
+router.put('/:id', updateTask);
+
+// Deleting user
+router.delete('/:id', deleteTask);
 
 export default router;
