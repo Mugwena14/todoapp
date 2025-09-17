fa0ebde0de7c5698b815e0c9758f2dcd08155e19
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-09-17 21:19:23 +0200

    Set the strict mode to be true

--- backend/models/tasksModel.js
+++ backend/models/tasksModel.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 const taskSchema = new mongoose.Schema({
     task: {type: String, required: true},
     name: {type: String, required: true, default: "Joe"}
-}, 
-    {strict: true}
-);
+});
 
 const Task = mongoose.model("Task", taskSchema);
 
